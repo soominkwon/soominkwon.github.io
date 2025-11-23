2773670d443808eab25320faa317f6f770340a16
soominkwon <soominkwon0402@gmail.com>
2025-11-23 22:33:32 +0000

    Deploying to gh-pages from @ soominkwon/soominkwon.github.io@092d67e66654ce64f8b6832cda67c602252242f9 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,14 +9,7 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-blog",
-          title: "blog",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/blog/";
-          },
-        },{id: "nav-cv",
+  },{id: "nav-cv",
           title: "cv",
           description: "",
           section: "Navigation",
